f9519c467f394d3a62498d7a31c4b7abbcd35aee
Marco Harald <araldus99@gmail.com>
2019-02-23 23:34:48 +0000

    initial website design

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -21,7 +21,7 @@ class Profile extends Component {
       phone:null,
       email:null,
       bio:"",
-      orgs:["Org1", "Org2", "Org3"],
+      orgs:["Do you spend too much money?", "Org2", "Org3"],
       evergreen:false
 
     }
@@ -44,32 +44,83 @@ class Profile extends Component {
   render() {
     return (
       <div className="Page addProfile">
-        <h5>
-          I am a charity profile
-          </h5>
-        <div>
-          <h5 className="uppercase">First Name</h5>
-          <input required type="text" className="" placeholder="First Name" value={this.state.firstname} onChange={(e) => this.handleChange('firstname',e)} />
+
+        <div id="headerBackground">
+          <h1>
+            Profile Report
+          </h1>    
         </div>
-        <div>
-          <h5 className="uppercase">Example</h5>
+
+        <div id="subHeaderBackground">
+          <h2>
+            Client Name
+          </h2> 
+        </div>
+
+        
+          <div id="bodyBackground">
+
+            <div id="leftBox">
+              <h7>
+                Answer to this first section
+              </h7>    
+            </div>
+
+            <div id="textBox">
+              <h7>
+                Answer to this first section
+              </h7>        
+            </div>
+
+
+
           <div>
-            <select value={this.state.org} onChange={ (e)=>this.handleChange('X',e)}>
-              <option value="" disabled selected>Select X</option>
-                {this.state.orgs.map(org=>
-                  <option value={org}>{org}</option>
-                )}
-            </select>
+            <h1></h1>
+          </div>
+
+          <div id="textBox">
+            <h7>
+              Place slider bar inside the cube
+            </h7>         
+          </div>
+
+          <div>
+            <h1></h1>
+          </div>
+            <div id="textBox">
+              <h5 className="uppercase">First Name</h5>
+              <input required type="text" className="" placeholder="What should we call you?" value={this.state.firstname} onChange={(e) => this.handleChange('firstname',e)} />
+            </div>         
+            <div>
+              <h5 className="uppercase">Example</h5>
+              <div>
+                <select value={this.state.org} onChange={ (e)=>this.handleChange('X',e)}>
+                  <option value="" disabled selected>Select X</option>
+                    {this.state.orgs.map(org=>
+                      <option value={org}>{org}</option>
+                    )}
+                </select>
+              </div>
+            </div>
+              <div> 
+                <h5 className="uppercase">Is this is a checkbox?</h5>
+                  <div id="textBox">
+                    <div id="checks">
+                      <h4>
+                        The First Tick Option
+                      </h4>
+                      <input type="checkbox" className="pt-input"  value={this.state.evergreen} onChange={(e) => this.handleChange('evergreen',e)} />
+                    </div> 
+                    <div id="checks">
+                      <h4>
+                        The Second Tick Option
+                      </h4>
+                      <input type="checkbox" className="pt-input"  value={this.state.evergreen} onChange={(e) => this.handleChange('evergreen',e)} />
+                    </div>
+                  </div>                            
+              </div>
           </div>
         </div>
-        <div>
-                            <h5 className="uppercase">Evergreen?</h5>
-                            <input type="checkbox" className="pt-input"  value={this.state.evergreen} onChange={(e) => this.handleChange('evergreen',e)} />
-                            
-                          </div>
-
-
-      </div>
     );
   }
 }
