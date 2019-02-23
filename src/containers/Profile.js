4b541c17ae5844eaffc7fcad52da2fb3bfcac8c1
Becks Simpson <becks.simpson@outlook.com>
2019-02-23 12:02:22 +0000

    Multiple form types

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -14,7 +14,9 @@ class Profile extends Component {
       image:null,
       phone:null,
       email:null,
-      bio:""
+      bio:"",
+      orgs:["Org1", "Org2", "Org3"],
+      evergreen:false
 
     }
     this.handleChange=this.handleChange.bind(this)
@@ -22,6 +24,12 @@ class Profile extends Component {
 
   handleChange(field,e){
     var value = e.target.value;
+
+    if(field=="evergreen"){
+      value=e.target.checked
+    }
+
+
     this.setState({
       [field]: value
     })
@@ -33,9 +41,28 @@ class Profile extends Component {
         <h5>
           I am a user profile
           </h5>
-        <h5 className="uppercase">First Name</h5>
-        <input required type="text" className="" placeholder="First Name" value={this.state.firstname} onChange={(e) => this.handleChange('firstname',e)} />
-                    
+        <div>
+          <h5 className="uppercase">First Name</h5>
+          <input required type="text" className="" placeholder="First Name" value={this.state.firstname} onChange={(e) => this.handleChange('firstname',e)} />
+        </div>
+        <div>
+          <h5 className="uppercase">Example</h5>
+          <div>
+            <select value={this.state.org} onChange={ (e)=>this.handleChange('X',e)}>
+              <option value="" disabled selected>Select X</option>
+                {this.state.orgs.map(org=>
+                  <option value={org}>{org}</option>
+                )}
+            </select>
+          </div>
+        </div>
+        <div>
+                            <h5 className="uppercase">Evergreen?</h5>
+                            <input type="checkbox" className="pt-input"  value={this.state.evergreen} onChange={(e) => this.handleChange('evergreen',e)} />
+                            
+                          </div>
+
+
       </div>
     );
   }
